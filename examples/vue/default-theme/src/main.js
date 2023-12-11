86f31526c229408b027e8f136612bc0de374634b
Haroen Viaene <hello@haroen.me>
2023-12-11 16:33:11 +0100

    chore(examples): migrate vue/default-theme to vite

--- examples/vue/default-theme/src/main.js
+++ examples/vue/default-theme/src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
-import InstantSearch from 'vue-instantsearch';
+import InstantSearch from 'vue-instantsearch/vue2/es';
 
+import './App.css';
 import App from './App.vue';
 
 Vue.use(InstantSearch);
-
-Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
